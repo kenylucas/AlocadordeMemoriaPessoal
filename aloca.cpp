2f6764dd1193aba17ef2d9bab4155d0516e55fa5
Keny Lucas <48927468+kenylucas@users.noreply.github.com>
2022-03-13 23:58:48 -0400

    Update aloca.cpp

--- aloca.cpp
+++ aloca.cpp
@@ -6,71 +6,69 @@ meualoc::meualoc(int tamanhoMemoria, int politicaMem)
     memoria = (char *)malloc(tamanhoMemoria);
     tamanho = tamanhoMemoria;
     politica = politicaMem;
-    livres = new Lista(tamanho, 0); // lista de livres
+    livres = new Lista(tamanho, 0); //lista de livres
     posicao = livres->prim();
 }
 
 char *meualoc::aloca(unsigned short int tamanho)
 {
     Bloco *bloco;
-    tamanho += 4; // cabeçalho de 4 bytes
+    tamanho += 4; //cabeçalho de 4 bytes
 
     // policitas
     if (politica == BESTFIT)
     {
-        // best fit
         Bloco *melhor = NULL;
         bloco = livres->prim();
-        while (bloco != NULL) // percorrer a lista toda
+        while (bloco != NULL) //percorrer a lista toda
         {
             if (bloco->tamanho >= tamanho)
-            {                                                           // fit
-                if (melhor == NULL || melhor->tamanho > bloco->tamanho) // best
+            {       
+                if (melhor == NULL || melhor->tamanho > bloco->tamanho)
                 {
                     melhor = bloco;
                 }
             }
-            bloco = bloco->obterProx(); // proximo bloco
+            bloco = bloco->obterProx(); //proximo bloco
         }
         if (melhor == NULL)
-        { // não achoy
+        { //não achoy
             return NULL;
         }
         else
-        { // achou o melhor
+        { //achou o melhor
             int pos = melhor->inicio;
             if (melhor->tamanho == tamanho)
-            {                        // se for igual, remover o bloco da lista de livres
-                livres->remove(pos); // remover da lista apenas o Bloco "aux"
+            {                        //se for igual, remover o bloco da lista de livres
+                livres->remove(pos); //remover da lista apenas o Bloco "aux"
             }
             else
-            { // se for maior, reduzir o tamanho do bloco "aux" na lista de livres
+            { //se for maior, reduzir o tamanho do bloco "aux" na lista de livres
                 melhor->inicio += tamanho;
                 melhor->tamanho -= tamanho;
             }
-            ///////////CABEÇALHO E ALOCAÇÃO///////////
-            tamanho -= 4; // remover tamanho do cabeçalho, pois já será escrito na memória
+            //Bitwise
+            tamanho -= 4; //remover tamanho do cabeçalho, pois já será escrito na memória
             memoria[pos++] = (MAGICNUMBER >> 8) & 0xFF;
             memoria[pos++] = MAGICNUMBER & 0xFF;
             memoria[pos++] = (tamanho >> 8) & 0xFF;
             memoria[pos++] = tamanho & 0xFF;
-            return &memoria[pos]; // retornar ponteiro para memoria alocada 4 = 2 / 2
+            return &memoria[pos]; //retornar ponteiro para memoria alocada 4 = 2 / 2
         }
     }
     else if (politica == NEXTFIT)
     {
-        // next fit
         bloco = posicao;
-        while (bloco != NULL && bloco->tamanho < tamanho) // tamanho insuficiente
+        while (bloco != NULL && bloco->tamanho < tamanho) //tamanho insuficiente
         {
-            bloco = bloco->obterProx(); // proximo bloco
+            bloco = bloco->obterProx(); //proximo bloco
         }
         if (bloco == NULL)
         {
             bloco = livres->prim();
-            while (bloco != NULL && bloco->tamanho < tamanho) // verifica tamanho insuficiente
+            while (bloco != NULL && bloco->tamanho < tamanho) //verifica tamanho insuficiente
             {
-                bloco = bloco->obterProx(); // proximo bloco
+                bloco = bloco->obterProx(); //proximo bloco
             }
             if (bloco == NULL)
             {
@@ -81,8 +79,8 @@ char *meualoc::aloca(unsigned short int tamanho)
         { // tamanho suficiente
             int pos = bloco->inicio;
             if (bloco->tamanho == tamanho)
-            {                                  // se for igual, remover o bloco da lista de livres
-                livres->remove(bloco->inicio); // remover da lista apenas o Bloco "aux"
+            {                                  //se for igual, remover o bloco da lista de livres
+                livres->remove(bloco->inicio); //remover da lista apenas o Bloco "aux"
                 posicao = bloco->obterProx() ? bloco->obterProx() : livres->prim();
             }
             else
@@ -91,47 +89,46 @@ char *meualoc::aloca(unsigned short int tamanho)
                 bloco->tamanho -= tamanho;
                 posicao = bloco;
             }
-            ///////////CABEÇALHO E ALOCAÇÃO///////////
-            tamanho -= 4; // remover tamanho do cabeçalho, pois já será escrito na memória
+            //Bitwise
+            tamanho -= 4; //remover tamanho do cabeçalho, pois já será escrito na memória
             memoria[pos++] = (MAGICNUMBER >> 8) & 0xFF;
             memoria[pos++] = MAGICNUMBER & 0xFF;
             memoria[pos++] = (tamanho >> 8) & 0xFF;
             memoria[pos++] = tamanho & 0xFF;
-            return &memoria[pos]; // retornar ponteiro para memoria alocada
+            return &memoria[pos]; //retornar ponteiro para memoria alocada
         }
         return NULL;
     }
     else
     {
-        // first fit
         bloco = livres->prim();
-        while (bloco != NULL && bloco->tamanho < tamanho) // tamanho insuficiente
+        while (bloco != NULL && bloco->tamanho < tamanho) //tamanho insuficiente
         {
-            bloco = bloco->obterProx(); // proximo bloco
+            bloco = bloco->obterProx(); //proximo bloco
         }
         if (bloco == NULL)
         {
             return NULL;
         }
         else
-        { // tamanho suficiente
+        { //tamanho suficiente
             int pos = bloco->inicio;
             if (bloco->tamanho == tamanho)
-            {                                  // se for igual, remover o bloco da lista de livres
-                livres->remove(bloco->inicio); // remover da lista apenas o Bloco "aux"
+            {                                  //se for igual, remover o bloco da lista de livres
+                livres->remove(bloco->inicio); //remover da lista apenas o Bloco "aux"
             }
             else
-            { // se for maior, reduzir o tamanho do bloco "aux" na lista de livres
+            { //se for maior, reduzir o tamanho do bloco "aux" na lista de livres
                 bloco->inicio = bloco->inicio + tamanho;
                 bloco->tamanho -= tamanho;
             }
-            ///////////CABEÇALHO E ALOCAÇÃO///////////
-            tamanho -= 4; // remover tamanho do cabeçalho, pois já será escrito na memória
+            //Bitwise
+            tamanho -= 4; //remover tamanho do cabeçalho, pois já será escrito na memória
             memoria[pos++] = (MAGICNUMBER >> 8) & 0xFF;
             memoria[pos++] = MAGICNUMBER & 0xFF;
             memoria[pos++] = (tamanho >> 8) & 0xFF;
             memoria[pos++] = tamanho & 0xFF;
-            return &memoria[pos]; // retornar ponteiro para memoria alocada
+            return &memoria[pos]; //retornar ponteiro para memoria alocada
         }
     }
 }
@@ -140,7 +137,7 @@ char *meualoc::verifica(char *ponteiro, int posicao)
 {
     char *cabecalho = ponteiro - 4;
     if (cabecalho < memoria || ponteiro > memoria + tamanho)
-        return NULL; // fora da memoria gerenciada
+        return NULL; //fora da memoria
 
     unsigned short magic = ((cabecalho[0] & 0xFF) << 8) + (cabecalho[1] & 0xFF);
     unsigned short tam = ((cabecalho[2] & 0xFF) << 8) + (cabecalho[3] & 0xFF);
@@ -154,12 +151,12 @@ int meualoc::libera(char *ponteiro)
 {
     char *cabecalho = ponteiro - 4;
     if (!ponteiro || cabecalho < memoria || ponteiro > memoria + tamanho)
-        return 0; // fora da memoria gerenciada
+        return 0; //fora da memoria
 
     unsigned short magic = ((cabecalho[0] & 0xFF) << 8) + (cabecalho[1] & 0xFF);
     if (magic == MAGICNUMBER)
     {
-        unsigned short tam = ((cabecalho[2] & 0xFF) << 8) + (cabecalho[3] & 0xFF) + 4; // + 4 do cabecalho
+        unsigned short tam = ((cabecalho[2] & 0xFF) << 8) + (cabecalho[3] & 0xFF) + 4; //+ 4 do cabecalho
         livres->inserir_ordenado(cabecalho - memoria,tam);
         for (int i = 0; i < 4; i++)
         {
@@ -176,7 +173,7 @@ void meualoc::imprimeDados()
     int cont = 0, maior, medio = 0;
     aux = livres->prim();
     maior = aux->tamanho;
-    while (aux != NULL) // percorrer a lista toda
+    while (aux != NULL) //percorrer a lista toda
     {
         cont += 1;
         medio += aux->tamanho;
@@ -196,7 +193,7 @@ string meualoc::imprimeDadosArquivo()
     int cont = 0, maior, medio = 0;
     aux = livres->prim();
     maior = aux->tamanho;
-    while (aux != NULL) // percorrer a lista toda
+    while (aux != NULL) //percorrer a lista toda
     {
         cont += 1;
         medio += aux->tamanho;
@@ -209,6 +206,6 @@ string meualoc::imprimeDadosArquivo()
     int totalEspacoLivre = medio;
     medio = medio / cont;
     string valor = to_string(cont) + "," + to_string(medio) + "," + to_string(totalEspacoLivre);
-    // cout << "elementosLitaVazio: " << cont << ", mediaTamanhoBlocos: " << medio << ", totalEspacoLivre: " << totalEspacoLivre << endl;
+    //cout << "elementosLitaVazio: " << cont << ", mediaTamanhoBlocos: " << medio << ", totalEspacoLivre: " << totalEspacoLivre << endl;
     return valor;
 }
